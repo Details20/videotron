3fe8eca58659788d3d45e2e8d1ef91cac90aeefb
Your Name <your_email@example.com>
2024-08-26 13:53:53 +0100

    first commit

---
+++ Videoton-main/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './Components/Navbar';
+import Login from './Components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Login/>
+    </div>
+  );
+}
+
+export default App;
